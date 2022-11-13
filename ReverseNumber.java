f61080e1b7f9cae1c15106a3611867d1e829d445
gaurav459433 <gaurav459433@gmail.com>
2022-11-13 23:32:50 +0530

    Reverse A Number Logic

---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.rfp212.assignday06reversenumber;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = scan.nextInt();
+        int a = 0;
+        for (int i = 0; n > 0; i++) {
+            a = (a + n % 10) * 10;
+            n /= 10;
+        }
+        System.out.println("Reverse of the Number is: " + (a / 10));
+
+    }
+}
